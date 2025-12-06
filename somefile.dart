bde8c9983fe18901286562833762e72bcc9eb06f
Joey Boerwinkel <joey@iconica.nl>
2025-12-06 23:12:33 +0100

    feat: add method to assign someone to a wish

--- somefile.dart
+++ somefile.dart
@@ -8,10 +8,18 @@ void startApp() {
 }
 
 class Wish {
+  WishResolver? assignee;
+}
+
+class WishResolver {
 
 }
 
 List<Wish> wishes = [];
 void createWish(Wish wish) {
   wishes.add(wish);
+}
+
+void assignWish(Wish wish, WishResolver resolver) {
+  wish.assignee = resolver;
 }
